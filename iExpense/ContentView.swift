af6e5302dda4b6f1e9d2ead568e6155923d8f10d
Daniil Kukhar <82108121+DaniilKukhar@users.noreply.github.com>
2023-01-10 00:50:42 +0200

    Add files of project

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Danya Kukhar on 09.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Danya Kukhar on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
